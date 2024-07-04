c10f01c3498e1a9fdb43d5ec45cb35a5dd51961d
Supremo1717 <ralphmatthewmanabat@gmail.com>
2024-07-04 17:17:14 +0800

    added links

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
 sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+
+
+// GMAIL
+
+const contactForm = document.getElementById('contact-form')
+const userName = document.getElementById('name').value
+const email = document.getElementById('email').value
+const subject = document.getElementById('subject').value
+const message = document.getElementById('message').value
+contactForm.addEventListener("submit", (e) => {
+	e.preventDefault()
+    // Construct the mailto: URL
+    var mailtoUrl = 'mailto:ralphmatthewmanabat@gmail.com?subject=' + encodeURIComponent(subject) + '&body=';
+    mailtoUrl += 'Name: ' + encodeURIComponent(userName) + '%0D%0A';
+    mailtoUrl += 'Email: ' + encodeURIComponent(email) + '%0D%0A';
+    mailtoUrl += 'Message: ' + encodeURIComponent(message);
+
+    // Open the user's default email client with the form data
+    window.location.href = mailtoUrl;
+})
